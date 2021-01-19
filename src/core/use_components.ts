d0ee4e894d6c740d4c22aeb444fdd35ca67e2419
无-言 <wangguodong194@163.com>
2021-01-19 17:10:40 +0800

    提交

--- src/core/use_components.ts
+++ src/core/use_components.ts
@@ -1,8 +1,8 @@
-import { Button, Col, Form, Input, message, Row } from "ant-design-vue"
+import { Button, Col, Form, Input, message, Row, Spin } from "ant-design-vue"
 import type { App } from "vue"
 
 export function useComponents(app: App) {
-  const components = [Button, Col, Form, Row, Input]
+  const components = [Button, Col, Form, Row, Input,Spin]
   components.forEach((component) => app.use(component))
   app.config.globalProperties.$message = message
   return app
--- src/views/Login/login.vue
+++ src/views/Login/login.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="login-wrapper">
     <div class="internationalization">
-      <a-dropdown v-model:visible="visible">
+      <a-dropdown v-model:visible="visibleRef">
         <global-outlined />
         <template #overlay>
           <a-menu @click="handleMenuClick">
@@ -18,109 +18,121 @@
         <p class="describe">Ant Design 是西湖区最具影响力的 Web 设计规范</p>
       </div>
       <div class="main">
-        <a-form layout="horizontal" :wrapper-col="{ span: 24 }">
-          <a-tabs :tab-bar-style="{ textAlign: 'center', borderBottom: 'unset' }">
-            <a-tab-pane key="1" tab="账号密码登录">
-              <a-form-item>
-                <a-input>
-                  <template #addonBefore>
-                    <user-outlined />
-                  </template>
-                </a-input>
-              </a-form-item>
-              <a-form-item>
-                <a-input-password>
-                  <template #addonBefore>
-                    <lock-outlined />
-                  </template>
-                </a-input-password>
-              </a-form-item>
-            </a-tab-pane>
-            <a-tab-pane key="2" tab="手机验证码登录">
-              <a-form-item>
-                <a-input>
-                  <template #addonBefore>
-                    <user-outlined />
-                  </template>
-                </a-input>
-              </a-form-item>
-              <a-form-item>
-                <a-input-password>
-                  <template #addonBefore>
-                    <lock-outlined />
-                  </template>
-                </a-input-password>
-              </a-form-item>
-            </a-tab-pane>
-          </a-tabs>
-        </a-form>
+        <a-spin :spinning="loadingRef">
+          <a-form layout="horizontal" :wrapper-col="{ span: 24 }" @submit="onSumbit">
+            <a-tabs :tab-bar-style="{ textAlign: 'center', borderBottom: 'unset' }">
+              <a-tab-pane key="1" tab="账号密码登录">
+                <a-form-item>
+                  <a-input>
+                    <template #addonBefore>
+                      <user-outlined />
+                    </template>
+                  </a-input>
+                </a-form-item>
+                <a-form-item>
+                  <a-input-password>
+                    <template #addonBefore>
+                      <lock-outlined />
+                    </template>
+                  </a-input-password>
+                </a-form-item>
+              </a-tab-pane>
+              <a-tab-pane key="2" tab="手机验证码登录">
+                <a-form-item>
+                  <a-input>
+                    <template #addonBefore>
+                      <user-outlined />
+                    </template>
+                  </a-input>
+                </a-form-item>
+                <a-form-item>
+                  <a-input-password>
+                    <template #addonBefore>
+                      <lock-outlined />
+                    </template>
+                  </a-input-password>
+                </a-form-item>
+              </a-tab-pane>
+            </a-tabs>
+            <a-form-item>
+              <a-button type="primary" @click="onSumbit">sumbit</a-button>
+            </a-form-item>
+          </a-form>
+        </a-spin>
       </div>
     </div>
   </div>
 </template>
 <script lang="ts">
-import { defineComponent, ref } from "vue";
-import { UserOutlined, LockOutlined, GlobalOutlined } from "@ant-design/icons-vue";
-import { Dropdown as ADropdown, Menu as AMenu, Tabs as ATabs } from "ant-design-vue";
-const AMenuItem = AMenu.Item;
-const ATabPane = ATabs.TabPane;
-export default defineComponent({
-  components: {
-    UserOutlined,
-    LockOutlined,
-    ADropdown,
-    GlobalOutlined,
-    AMenu,
-    AMenuItem,
-    ATabs,
-    ATabPane,
-  },
-  setup() {
-    let visible = ref(false);
-    return { visible };
-  },
-  methods: {
-    handleMenuClick() {},
-  },
-});
+  import { defineComponent, ref } from "vue"
+  import { UserOutlined, LockOutlined, GlobalOutlined } from "@ant-design/icons-vue"
+  import { Dropdown as ADropdown, Menu as AMenu, Tabs as ATabs } from "ant-design-vue"
+  const AMenuItem = AMenu.Item
+  const ATabPane = ATabs.TabPane
+  export default defineComponent({
+    components: {
+      UserOutlined,
+      LockOutlined,
+      ADropdown,
+      GlobalOutlined,
+      AMenu,
+      AMenuItem,
+      ATabs,
+      ATabPane,
+    },
+    setup(props) {
+      let visibleRef = ref(false)
+      let loadingRef = ref(false)
+      return { visibleRef, loadingRef, }
+    },
+    methods: {
+      onSumbit() {
+        this.loadingRef = true
+        setTimeout(() => {
+          (this.loadingRef = false), this.$message.success("啦啦啦")
+        }, 2000)
+      },
+    },
+  })
 </script>
 <style lang="scss">
-.login-wrapper {
-}
-.internationalization {
-  text-align: right;
-  margin-right: 25px;
-  padding: 10px;
-  font-size: 1.2rem;
-}
-.login-content {
-  .login-logo {
-    padding: 30px;
-    text-align: center;
+  .login-wrapper {
+  }
+  .internationalization {
+    text-align: right;
+    margin-right: 25px;
+    padding: 10px;
+    font-size: 1.2rem;
+  }
+  .login-content {
+    padding-top: 5%;
+    .login-logo {
+      padding: 50px;
+      text-align: center;
 
-    img {
-      height: 44px;
-    }
-    .title {
-      padding: 10px;
-      font-size: 2rem;
-      color: rgba(0, 0, 0, 0.85);
-      font-family: Avenir, Helvetica Neue, Arial, Helvetica, sans-serif;
-      font-weight: 600;
-      position: relative;
-      top: 2px;
+      img {
+        height: 44px;
+      }
+      .title {
+        padding: 10px;
+        font-size: 2rem;
+        color: rgba(0, 0, 0, 0.85);
+        font-family: Avenir, Helvetica Neue, Arial, Helvetica, sans-serif;
+        font-weight: 600;
+        position: relative;
+        top: 2px;
+      }
+
+      .describe {
+        opacity: 0.8;
+        padding-top: 10px;
+      }
     }
 
-    .describe {
-      opacity: 0.8;
-      padding-top: 10px;
+    .main {
+      min-width: 260px;
+      width: 368px;
+      margin: 0 auto;
     }
   }
-
-  .main {
-    min-width: 260px;
-    width: 368px;
-    margin: 0 auto;
-  }
-}
 </style>
